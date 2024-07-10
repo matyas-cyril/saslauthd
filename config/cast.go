eecaf4cf6b7db45a552e1df3b5e2c93824edd918
matyas.cyril <matyas.cyril@gmail.com>
2024-07-10 17:21:34 +0200

    Initial commit

---
+++ config/cast.go
@@ -0,0 +1,59 @@
+package config
+
+import (
+	"fmt"
+	"strings"
+)
+
+func castString(p any) (str string, err error) {
+
+	defer func() {
+		if p := recover(); p != nil {
+			str = ""
+			err = fmt.Errorf("param type not valid to cast to string")
+		}
+	}()
+
+	str = strings.TrimSpace(p.(string))
+	if len(str) == 0 {
+		return "", fmt.Errorf("string length equal zero")
+	}
+
+	return str, nil
+}
+
+func castUint8(v any) (nbr uint8, err error) {
+
+	defer func() {
+		if p := recover(); p != nil {
+			nbr = 0
+			err = fmt.Errorf("param type not valid to cast to uint8")
+		}
+	}()
+
+	return uint8(v.(int64)), nil
+}
+
+func castUint16(v any) (nbr uint16, err error) {
+
+	defer func() {
+		if p := recover(); p != nil {
+			nbr = 0
+			err = fmt.Errorf("param type not valid to cast to uint16")
+		}
+	}()
+
+	return uint16(v.(int64)), nil
+}
+
+func castUint32(v any) (nbr uint32, err error) {
+
+	defer func() {
+		if p := recover(); p != nil {
+			nbr = 0
+			err = fmt.Errorf("param type not valid to cast to uint32")
+		}
+	}()
+
+	return uint32(v.(int64)), nil
+}
