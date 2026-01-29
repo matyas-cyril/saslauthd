79547e04adff56f3f944e660702db328fb7c547f
matyas.cyril <matyas.cyril@gmail.com>
2026-01-29 10:16:21 +0100

    Modification du Test Local

--- cache_test.go
+++ cache_test.go
@@ -7,14 +7,26 @@ import (
 	"github.com/matyas-cyril/saslauthd/cache_generic"
 )
 
+var PATH string = "/tmp"
+
 // go test -timeout 5s -run ^TestLocal$
 func TestLocal(t *testing.T) {
-	opt := []any{"/tmp"}
+	opt := []any{PATH}
 	c, err := cache_generic.New("LOCAL", nil, 3, 3, opt)
 	if err != nil {
 		t.Fatal(err)
 	}
-	fmt.Println(c)
+
+	data := map[string][]byte{
+		"hello": []byte("world"),
+	}
+
+	hash := "68b329da9893e34099c7d8ad5cb9c940"
+
+	if err := c.SetSucces(data, []byte(hash)); err != nil {
+		t.Fatal(err)
+	}
+
 }
 
 // go test -timeout 5s -run ^TestMemcache$
