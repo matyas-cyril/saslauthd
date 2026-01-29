8920eb66d80d55187ddd17d451facc6e186f8cdb
matyas.cyril <matyas.cyril@gmail.com>
2026-01-29 14:51:06 +0100

    Pris en compte de memcache, redis et keydb dans les tests

--- cache_test.go
+++ cache_test.go
@@ -9,6 +9,12 @@ import (
 
 var PATH string = "/tmp"
 
+var DATA = map[string][]byte{
+	"hello": []byte("world"),
+}
+
+var HASH []byte = []byte("68b329da9893e34099c7d8ad5cb9c940")
+
 // go test -timeout 5s -run ^TestLocal$
 func TestLocal(t *testing.T) {
 
@@ -22,17 +28,11 @@ func TestLocal(t *testing.T) {
 	}
 	defer c.Close()
 
-	data := map[string][]byte{
-		"hello": []byte("world"),
-	}
-
-	hash := "68b329da9893e34099c7d8ad5cb9c940"
-
-	if err := c.SetSucces(data, []byte(hash)); err != nil {
+	if err := c.SetSucces(DATA, []byte(HASH)); err != nil {
 		t.Fatal(err)
 	}
 
-	rst, err := c.GetCache([]byte(hash))
+	rst, err := c.GetCache(HASH)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -40,26 +40,56 @@ func TestLocal(t *testing.T) {
 	fmt.Println("Cache -> ", rst)
 }
 
-/*
 // go test -timeout 5s -run ^TestMemcache$
 func TestMemcache(t *testing.T) {
-	opt := []any{"127.0.0.1", 11211, 10}
 
-	c, err := cache_generic.New("MEMCACHE", nil, 3, 3, opt)
+	opt := map[string]any{
+		"host":    "127.0.0.1",
+		"port":    11211,
+		"timeout": 10,
+	}
+
+	c, err := cache.New("MEMCACHE", nil, 3, 3, opt)
 	if err != nil {
 		t.Fatal(err)
 	}
-	fmt.Println(c)
+
+	if err := c.SetSucces(DATA, HASH); err != nil {
+		t.Fatal(err)
+	}
+
+	rst, err := c.GetCache(HASH)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	fmt.Println("Cache -> ", rst)
+
 }
 
 // go test -timeout 5s -run ^TestRedis$
 func TestRedis(t *testing.T) {
-	opt := []any{"127.0.0.1", 6379, 10}
 
-	c, err := cache_generic.New("REDIS", nil, 3, 3, opt)
+	opt := map[string]any{
+		"host":    "127.0.0.1",
+		"port":    6379,
+		"timeout": 10,
+		"db":      0,
+	}
+
+	c, err := cache.New("REDIS", nil, 3, 3, opt)
 	if err != nil {
 		t.Fatal(err)
 	}
-	fmt.Println(c)
+
+	if err := c.SetSucces(DATA, HASH); err != nil {
+		t.Fatal(err)
+	}
+
+	rst, err := c.GetCache(HASH)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	fmt.Println("Cache -> ", rst)
 }
-*/
