088f0144e61d16de66a7bf274545d7defee72d9f
matyas.cyril <matyas.cyril@gmail.com>
2026-02-02 16:06:10 +0100

    Convertir les paramètres en structure exploitable

--- src_plugin/ldap/ldap.go
+++ src_plugin/ldap/ldap.go
@@ -5,6 +5,10 @@ import (
 	"encoding/gob"
 	"fmt"
 	"io"
+	"reflect"
+	"strings"
+
+	ldap "saslauthd/plugin/ldap/ldap"
 )
 
 func Check(opt map[string]any) (buffer bytes.Buffer, err error) {
@@ -39,7 +43,7 @@ func Auth(data map[string][]byte, args bytes.Buffer) (valid bool, err error) {
 		}
 	}()
 
-	var arg ldapStruct
+	var arg ldap.LdapOpt
 
 	dec := gob.NewDecoder(&args)
 	if err := dec.Decode(&arg); err != nil && err != io.EOF {
@@ -50,9 +54,93 @@ func Auth(data map[string][]byte, args bytes.Buffer) (valid bool, err error) {
 
 }
 
-type ldapStruct struct {
-}
+func interfaceToStruct(data map[string]any) (*ldap.LdapOpt, error) {
 
-func interfaceToStruct(data map[string]any) (*ldapStruct, error) {
-	return nil, nil
+	// Déclarations et valeurs par défaut
+	var (
+		uri           string
+		admin         string
+		passwd        string
+		baseDN        string
+		port          uint16 = 389
+		timeout       uint16 = 10
+		tls           bool   = false
+		tlsSkipVerify bool   = true
+	)
+
+	for k, v := range data {
+
+		switch k {
+
+		case "uri", "admin", "pwd", "baseDN":
+			kV, kErr := v.(string)
+			if !kErr {
+				return nil, fmt.Errorf("ldap key '%s' failed to typecast", k)
+			}
+			kV = strings.TrimSpace(kV)
+			if len(kV) == 0 {
+				return nil, fmt.Errorf("ldap key '%s' defined but empty", k)
+			}
+
+			switch k {
+
+			case "uri":
+				uri = kV
+			case "admin":
+				admin = kV
+			case "pwd":
+				passwd = kV
+			case "baseDN":
+				baseDN = kV
+			}
+
+		case "port", "timeout":
+			typeTarget := reflect.TypeFor[int]()
+			rv := reflect.ValueOf(data[k])
+			if !rv.Type().AssignableTo(typeTarget) {
+				return nil, fmt.Errorf("ldap key '%s' failed to typecast", k)
+			}
+
+			nbr := rv.Convert(typeTarget).Int()
+			if nbr < 0 || nbr > 65535 {
+				return nil, fmt.Errorf("ldap key '%s' integer range invalid", k)
+			}
+
+			switch k {
+			case "port":
+				port = uint16(nbr)
+			case "timeout":
+				timeout = uint16(nbr)
+			}
+
+		case "tls", "tlsSkipVerify":
+			kV, kErr := v.(bool)
+			if !kErr {
+				return nil, fmt.Errorf("ldap key '%s' failed to typecast", k)
+			}
+
+			switch k {
+			case "tls":
+				tls = kV
+			case "tlsSkipVerify":
+				tlsSkipVerify = kV
+			}
+
+		default:
+			return nil, fmt.Errorf("ldap key '%s' not exist", k)
+
+		}
+
+	}
+
+	return &ldap.LdapOpt{
+		Uri:                uri,
+		Port:               port,
+		Admin:              admin,
+		Passwd:             passwd,
+		BaseDn:             baseDN,
+		Timeout:            timeout,
+		Tls:                tls,
+		InsecureSkipVerify: tlsSkipVerify,
+	}, nil
 }
