f9b31acedb4b1b2e90b80868e2110035e09b1502
matyas.cyril <matyas.cyril@gmail.com>
2026-02-04 16:19:26 +0100

    Ajout du test auth ldap

--- src_plugin/ldap/ldap_test.go
+++ src_plugin/ldap/ldap_test.go
@@ -1,6 +1,7 @@
 package main_test
 
 import (
+	"fmt"
 	"testing"
 
 	ldap "saslauthd/plugin/ldap"
@@ -9,6 +10,25 @@ import (
 // go test -timeout 5s -run ^TestLdap$
 func TestLdap(t *testing.T) {
 
-	ldap.Check(nil)
+	opt := make(map[string]any)
+	opt["uri"] = "172.0.10.17"
+	opt["admin"] = "cn=admin,dc=example,dc=com"
+	opt["pwd"] = "AdminLDAP"
+	opt["baseDN"] = "ou=people,dc=example,dc=com"
+
+	data, err := ldap.Check(opt)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	login := make(map[string][]byte)
+	login["d0"] = []byte("cyril")
+	login["d1"] = []byte("Cyril")
+
+	auth, err := ldap.Auth(login, data)
+	if err != nil {
+		t.Fatal(err)
+	}
+	fmt.Println(auth)
 
 }
